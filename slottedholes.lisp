b5689d2a5db42c74f96b5b3754c21bfe1b4b5b75
tdrules <d.w.evenson@gmail.com>
2013-10-08 16:12:28 -0500

    Begin initial structure

--- slottedholes.lisp
+++ slottedholes.lisp
@@ -7,7 +7,13 @@
 
 	(setq center (getpoint "\nChoose center point:") )
 	
-	(setq radius (getreal  "\nInput radius:") )
+	(setq radius (getreal  "\nInput radius(inches):") )
+	
+	(setq len0 (getreal "\nInput total length(inches):") )
+	
+	(setq p1 (- 
+	
+	(setq len (
 	
 	(prin1 center)
 	
